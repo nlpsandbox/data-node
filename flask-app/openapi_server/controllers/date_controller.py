764d7b4f23ad26321a83d906ccd6bd24f716e962
George Kowalski <gkowalski@mcw.edu>
2020-09-09 16:08:51 -0500

    Added /annotations/dates  Rest call

--- flask-app/openapi_server/controllers/date_controller.py
+++ flask-app/openapi_server/controllers/date_controller.py
@@ -4,9 +4,10 @@ import six
 from openapi_server.models.date_annotation import DateAnnotation  # noqa: E501
 from openapi_server.models.note import Note  # noqa: E501
 from openapi_server import util
+import openapi_server.db_connection as db
+import json
 
-
-def dates_read_all(note=None):  # noqa: E501
+def dates_read_all( ):  # noqa: E501
     """Get all date annotations
 
     Returns the date annotations # noqa: E501
@@ -16,6 +17,30 @@ def dates_read_all(note=None):  # noqa: E501
 
     :rtype: List[DateAnnotation]
     """
-    if connexion.request.is_json:
-        note = [Note.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
-    return 'do some magic!'
+    # if connexion.request.is_json:
+    #     note = [Note.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
+    # return 'do some magic!'
+
+    """Get a clinical note by ID
+
+     Returns the clinical note for a given ID # noqa: E501
+
+     :param id: The ID of the clinical note to fetch
+     :type id: str
+
+     :rtype: Note
+     """
+    values = db.load_config()
+
+    conn = db.get_connection_local_pg(values)
+    cur = conn.cursor()
+    select_anno = 'SELECT id, pat_note_id , start ,  stop - start as len, text, category, type from  i2b2_data.public.pat_annotations where category = \'DATE\' '
+    cur.execute(select_anno  )
+    all_rows = cur.fetchall()
+    res = []
+    for row in all_rows:
+        id = row[0]
+        dict = {'id': id, 'noteId': row[1] , 'start': str(row[2]) ,  'length': str(row[3]) , 'text': str(row[4]),  'category': str(row[5]),  'type': str(row[6])}
+        res.append(dict)
+
+    return json.dumps(res)

886aab6db9cccb0afb910b54f32d13af41a8e97c
tschaffter <thomas.schaffter@gmail.com>
2020-11-18 15:11:48 -0800

    Fix docker-compose setup

--- server/openapi_server/__main__.py
+++ server/openapi_server/__main__.py
@@ -13,23 +13,16 @@ app.add_api('openapi.yaml',
             arguments={'title': 'NLP Sandbox Data Node API'},
             pythonic_params=True)
 
-print(f"username: {config().db_username}")
-print(f"password: {config().db_password}")
-
 connect(
     db=config().db_database,
     username=config().db_username,
     password=config().db_password,
-    host=config().db_host  # 'mongodb://localhost:27017'
+    host=config().db_host
 )
-
-# print(f" IS NOW RUNNING")
-# print(f"port: {config().server_port}")
 
 
 def main():
     # TODO: Consider using param host="0.0.0.0", debug=True,
-    print("SHOULD NOT BE HERE WITH UWSGI")
     app.run(port=config().server_port)
 
 

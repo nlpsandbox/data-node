0a80635e0d807060a4be547074b3c05746b3a1de
Thomas Schaffter <thomas.schaffter@gmail.com>
2020-12-03 17:47:43 -0800

    Update codebase based on latest spec (#102)

--- server/openapi_server/models/service.py
+++ server/openapi_server/models/service.py
@@ -155,6 +155,12 @@ class Service(Model):
         :param license: The license of this Service.
         :type license: str
         """
+        allowed_values = ["afl-3.0", "apache-2.0", "artistic-2.0", "bsl-1.0", "bsd-2-clause", "bsd-3-clause", "bsd-3-clause-clear", "cc", "cc0-1.0", "cc-by-4.0", "cc-by-sa-4.0", "wtfpl", "ecl-2.0", "epl-1.0", "epl-2.0", "eupl-1.1", "agpl-3.0", "gpl", "gpl-2.0", "gpl-3.0", "lgpl", "lgpl-2.1", "lgpl-3.0", "isc", "lppl-1.3c", "ms-pl", "mit", "mpl-2.0", "osl-3.0", "postgresql", "ofl-1.1", "ncsa", "unlicense", "zlib"]  # noqa: E501
+        if license not in allowed_values:
+            raise ValueError(
+                "Invalid value for `license` ({0}), must be one of {1}"
+                .format(license, allowed_values)
+            )
 
         self._license = license
 

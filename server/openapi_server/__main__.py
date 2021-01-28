60e876a69087c1c085a1457661cccd00c4cdc00f
tschaffter <thomas.schaffter@gmail.com>
2021-01-28 09:51:48 -0800

    Remove redirection to /

--- server/openapi_server/__main__.py
+++ server/openapi_server/__main__.py
@@ -12,9 +12,6 @@ app = connexion.App(__name__, specification_dir='./openapi/')
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('openapi.yaml', pythonic_params=True)
 
-# Configuring / to return the service information (required by NLP Sandbox)
-app.add_url_rule('/', 'root', lambda: flask.redirect('/api/v1/service'))
-
 connect(
     db=config().db_database,
     username=config().db_username,

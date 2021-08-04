af1ad4f2ef9fd696a09d6600edbb534c7fbf8c0f
thomasyu888 <thomas.yu@sagebase.org>
2021-08-04 10:33:44 +0800

    Date format is not required

--- server/openapi_server/dbmodels/text_date_annotation.py
+++ server/openapi_server/dbmodels/text_date_annotation.py
@@ -6,7 +6,7 @@ class TextDateAnnotation(EmbeddedDocument):
     length = IntField(required=True)
     text = StringField(required=True)
     confidence = FloatField(required=True)
-    dateFormat = StringField(required=True)
+    dateFormat = StringField(required=False)
 
     def to_dict(self):
         doc = self.to_mongo().to_dict()
